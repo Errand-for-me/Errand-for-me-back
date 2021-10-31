c35568d5cabe3407944c6c8e383e4fbd12f0ce7a
sumin123456 <soomin5234@gmail.com>
2021-10-31 14:21:14 +0900

    [feat] 게시판 구현
    게시판 구현

--- src/main/java/com/errand/project/web/BoardController.java
+++ src/main/java/com/errand/project/web/BoardController.java
@@ -4,6 +4,7 @@ import com.errand.project.domain.board.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Map;
@@ -20,15 +21,26 @@ public class BoardController {
         return boardService.findAll();
     }
 
+    @RequestMapping(value="/board/write", method = RequestMethod.GET)
+    public String boardWrite(@RequestParam String title, @RequestParam String content) {
+        boardService.save(title, content);
+
+        RedirectView redirect = new RedirectView();
+        redirect.setUrl("bulletin");
+        return "redirect:/";
+    }
+
     @PostMapping("/board")
-    public String boardPost(@RequestBody MultiValueMap<String, String> requestBody) {
+    public RedirectView boardPost(@RequestBody MultiValueMap<String, String> requestBody) {
 
         String title = requestBody.get("title").get(0);
         String content = requestBody.get("content").get(0);
 
         boardService.save(title, content);
 
-        return "redirect:/bulletin";
+        RedirectView redirect = new RedirectView();
+        redirect.setUrl("");
+        return redirect;
     }
 
 }
