8407e90325b1cb8af59863f68eb703427be72c15
sumin123456 <soomin5234@gmail.com>
2021-11-14 22:26:29 +0900

    [feat] 퀘스트 이미지 추가 기능

--- src/main/java/com/errand/project/web/QuestController.java
+++ src/main/java/com/errand/project/web/QuestController.java
@@ -3,10 +3,17 @@ package com.errand.project.web;
 import com.errand.project.domain.quest.Quest;
 import com.errand.project.domain.quest.registerQuest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,16 +23,58 @@ public class QuestController {
 
     private final QuestService questService;
 
+    @GetMapping("/img/{filename}")
+    public ResponseEntity<Resource> viewImg(@PathVariable("filename") String imgname) throws IOException {
+        String _path = System.getProperty("user.dir");
+        String inputFile = _path + "/img/" + imgname;
+        Path path = new File(inputFile).toPath();
+        FileSystemResource resource = new FileSystemResource(path);
+
+        if(!resource.exists()) {
+            inputFile = _path + "/img/none.svg";
+            path = new File(inputFile).toPath();
+            resource = new FileSystemResource(path);
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(Files.probeContentType(path)))
+                .body(resource);
+    }
+
     @GetMapping("/quest")
     public List<Quest> questGet() { return questService.findAll(); }
 
     @PostMapping("/quest")
     public RedirectView questPost(@ModelAttribute registerQuest request) {
-        System.out.println(request.getPeople());
-        System.out.println(request.getImage().getOriginalFilename());
-        System.out.println(request.getTitle());
+        MultipartFile file = request.getImage();
+        StringBuilder sb = new StringBuilder();
 
-        MultipartFile file = request.getImage();;
+        String title = request.getTitle();
+        String content = request.getContent();
+        int people = request.getPeople();
+        String filename = request.getImage().getOriginalFilename();
+        if (filename == "") filename = "none.svg";
+        String writer = "임시 유저 이름";
+
+        questService.save(title, content, people, filename, writer);
+
+        if (file.isEmpty()) {
+            sb.append("none");
+        } else {
+            sb.append(file.getOriginalFilename());
+        }
+
+        if (!file.isEmpty()) {
+            String path = System.getProperty("user.dir");
+            File dest = new File(path + "/img/" + sb.toString());
+            try {
+                file.transferTo(dest);
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         RedirectView redirect = new RedirectView();
         redirect.setUrl("");
